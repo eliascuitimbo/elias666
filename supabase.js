966b3a2f834aab915ee65c67b86f0b46d039c733
eliascuitimbo <kanesson1984@gmail.com>
2025-04-15 23:00:20 -0500

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://kctitomnuvrmdumrybes.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjdGl0b21udXZybWR1bXJ5YmVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3NzA4OTcsImV4cCI6MjA2MDM0Njg5N30.syXuvkyaxoQWcJx0xhSUXhmrI1yOGF5SFZADd7lGEts";
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
